09b736e2490697d84f9d201d31d4d0b2de10f777
HwrG90 <HwrG90@gmail.com>
2022-03-02 15:54:44 -0300

    formulario funcionando

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+const header = document.createElement("header");
+header.innerHTML = "<h1>Desafio JavaScript</h1>";
+
+const body = document.body;
+
+body.appendChild(header);
+
 let input1 = document.getElementById("nombre");
 let input2 = document.getElementById("apellido");
-let input3 = document.getElementById("nota");
+let input3 = document.getElementById("notaUno");
+let input4 = document.getElementById("notaDos");
+let input5 = document.getElementById("notaTres");
+
 input1.onkeyup = () => {
     console.log("keyUp");
 };
@@ -10,6 +20,16 @@ input2.onkeydown = () => {
 input3.onkeyup = () => {
     console.log("keyUp");
 };
+input4.onkeydown = () => {
+    console.log("keyUp");
+};
+input5.onkeyup = () => {
+    console.log("keyUp");
+};
+
+function redondeo(valor) {
+    return Math.round(valor);
+}
 
 let formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", validarFormulario);
@@ -17,99 +37,67 @@ formulario.addEventListener("submit", validarFormulario);
 function validarFormulario(e) {
     e.preventDefault();
     console.log("Calculando notas");
-}
 
-const header = document.createElement("header");
-header.innerHTML = "<h1>Desafio JavaScript</h1>";
+    for (let i = 1; i <= 1; i += 1) {
+        let nombre = document.getElementById("nombre").value;
+        let apellido = document.getElementById("apellido").value;
+        let notaUno = document.getElementById("notaUno").value;
 
-const body = document.body;
+        let notaDos = document.getElementById("notaDos").value;
 
-body.appendChild(header);
+        let notaTres = document.getElementById("notaTres").value;
 
-function redondeo(valor) {
-    return Math.round(valor);
-}
+        let total = Number(notaUno) + Number(notaDos) + Number(notaTres) / 3;
+        //var resultado = Number(numero1)+Number(numero2);
 
-for (let i = 1; i <= 3; i += 1) {
-    let ingresarNombre = prompt("Ingresar Nombre");
-    let apellidoIngresado = prompt("Ingresar Apellido");
-    let ingresarNota1 = parseFloat(prompt("Ingresar Nota 1"));
-    let ingresarNota2 = parseFloat(prompt("Ingresar Nota 2"));
-    let ingresarNota3 = parseFloat(prompt("Ingresar Nota 3"));
-    let total = parseInt(ingresarNota1 + ingresarNota2 + ingresarNota3);
+        if (!isNaN(nombre, apellido)) {
+            alert("Ingresar nombres con letras!");
+        }
+        if (Number.isNaN(notaUno, notaDos, notaTres)) {
+            alert("Ingresar notas numericas!");
+        }
 
-    if (!isNaN(ingresarNombre, apellidoIngresado)) {
-        alert("Ingresar nombres con letras!");
-    }
-    if (Number.isNaN(ingresarNota1, ingresarNota2, ingresarNota3)) {
-        alert("Ingresar notas numericas!");
-    }
+        /*   alert(
+                ` Alumnno  N° ${i}\n Nombre: ${ingresarNombre}\n Apellido: ${apellidoIngresado}\n Nota 1: ${ingresarNota1}\n Nota 2: ${ingresarNota2}\n Nota 3: ${ingresarNota3}\n Resultado de Notas : ${
+            ingresarNota1 + ingresarNota2 + ingresarNota3
+            }\n Nota Final : ${redondeo(total / 3)}`
+              ); */
 
-    alert(
-        ` Alumnno  N° ${i}\n Nombre: ${ingresarNombre}\n Apellido: ${apellidoIngresado}\n Nota 1: ${ingresarNota1}\n Nota 2: ${ingresarNota2}\n Nota 3: ${ingresarNota3}\n Resultado de Notas : ${
-    ingresarNota1 + ingresarNota2 + ingresarNota3
-    }\n Nota Final : ${redondeo(total / 3)}`
-    );
+        console.log(
+            ` Alumnno  N° ${i}\n Nombre: ${nombre}\n Apellido: ${apellido}\n Nota 1: ${notaUno}\n Nota 2: ${notaDos}\n Nota 3: ${notaTres}\n Nota Final : ${redondeo(
+        total / 3
+        )}`
+        );
 
-    console.log(
-        ` Alumnno  N° ${i}\n Nombre: ${ingresarNombre}\n Apellido: ${apellidoIngresado}\n Nota 1: ${ingresarNota1}\n Nota 2: ${ingresarNota2}\n Nota 3: ${ingresarNota3}\n Resultado de Notas : ${
-    ingresarNota1 + ingresarNota2 + ingresarNota3
-    }\n Nota Final : ${redondeo(total / 3)}`
-    );
+        let personas = [
+            `Nombre: ${nombre}`,
+            `Apellido: ${apellido}`,
+            `Nota 1: ${notaUno}`,
+            `Nota 2: ${notaDos}`,
+            `Nota 3: ${notaTres}`,
+            `Nota final: ${redondeo(total / 3)}`,
+            "<hr>",
+        ];
 
-    /*    document.write(
-            " Alumnno  N° " +
-            i +
-        "<br>" +
-        " Nombre: " +
-        ingresarNombre +
-        "<br>" +
-        " Apellido: " +
-        apellidoIngresado +
-        "<br>" +
-        " Nota 1: " +
-        ingresarNota1 +
-        "<br>" +
-        " Nota 2: " +
-        ingresarNota2 +
-        "<br>" +
-        " Nota 3: " +
-        ingresarNota3 +
-        "<br>" +
-        " Resultado de Notas : " +
-        (ingresarNota1 + ingresarNota2 + ingresarNota3) +
-        "<br>" +
-        " Nota Final : " +
-        redondeo(total / 3) +
-        "<br>" +
-        "<hr>"
-        ); */
+        const alumnos = [{
+            id: 0,
+            Nombre: nombre,
+            apellido: apellido,
+            Nota: redondeo(total / 3),
+        }, ];
 
-    const alumnos = [{
-        id: 0,
-        Nombre: ingresarNombre,
-        apellido: apellidoIngresado,
-        Nota: redondeo(total / 3),
-    }, ];
+        const aprobados = alumnos.filter((alumnos) => alumnos.Nota >= 6);
+        console.log(aprobados);
 
-    const aprobados = alumnos.filter((alumnos) => alumnos.Nota >= 6);
-    console.log(aprobados);
+        const desaprobados = alumnos.filter((alumnos) => alumnos.Nota <= 5);
+        console.log(desaprobados);
 
-    const desaprobados = alumnos.filter((alumnos) => alumnos.Nota <= 5);
-    console.log(desaprobados);
+        let list = document.getElementById("alumnos");
 
-    let list = document.getElementById("alumnos");
-
-    let personas = [
-        "Nombre:" + " " + ingresarNombre,
-        "Apellido:" + " " + apellidoIngresado,
-        `Nota final: ${redondeo(total / 3)}`,
-        "<hr>",
-    ];
-
-    for (const persona of personas) {
-        let li = document.createElement("li");
-        li.innerHTML = persona;
-        list.appendChild(li);
+        for (const persona of personas) {
+            let li = document.createElement("li");
+            li.innerHTML = persona;
+            list.appendChild(li);
+        }
     }
 }
